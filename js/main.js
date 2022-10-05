59bc752a1e90255eea82cf3f956d45f46ec4c45a
KamilS99 <kamilskowron99@gmail.com>
2022-10-05 21:56:43 +0200

    site created

--- js/main.js
+++ js/main.js
@@ -1,8 +1,81 @@
 const hamburger = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav-mobile')
-
-const handleNavMobile = () => {
-    hamburger.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
+const navMobileLinks = document.querySelectorAll('.nav-mobile__link')
+const cakes = document.querySelectorAll('.offer__cake')
+const pies = document.querySelectorAll('.offer__pie')
+const sortAllBtn = document.querySelector('.offer__sort-btn-all')
+const sortCakesBtn = document.querySelector('.offer__sort-btn-cakes')
+const sortPiesBtn = document.querySelector('.offer__sort-btn-pies')
+const footerYear = document.querySelector('.footer__year')
+//THROTTLING
+const throttle = (fn, delay) => {
+	let last = 0
+	return () => {
+		const now = new Date().getTime()
+		if (now - last < delay) {
+			return
+		}
+		last = now
+		return fn()
+	}
 }
+//SORT OFFER PRODUCTS
+const sortAllProducts = () => {
+	cakes.forEach(cake => {
+		cake.style.display = 'block'
+	})
+	pies.forEach(pie => {
+		pie.style.display = 'block'
+	})
+	sortCakesBtn.classList.remove('offer__sort-btn--active')
+	sortPiesBtn.classList.remove('offer__sort-btn--active')
+	sortAllBtn.classList.add('offer__sort-btn--active')
+}
+const sortCakes = () => {
+	cakes.forEach(cake => {
+		cake.style.display = 'block'
+	})
+	pies.forEach(pie => {
+		pie.style.display = 'none'
+	})
+	sortCakesBtn.classList.add('offer__sort-btn--active')
+	sortPiesBtn.classList.remove('offer__sort-btn--active')
+	sortAllBtn.classList.remove('offer__sort-btn--active')
+}
+const sortPies = () => {
+	cakes.forEach(cake => {
+		cake.style.display = 'none'
+	})
+	pies.forEach(pie => {
+		pie.style.display = 'block'
+	})
+	sortCakesBtn.classList.remove('offer__sort-btn--active')
+	sortPiesBtn.classList.add('offer__sort-btn--active')
+	sortAllBtn.classList.remove('offer__sort-btn--active')
+}
+//CURRENT YEAR FOR FOOTER 
+const year = new Date().getFullYear()
+footerYear.textContent = year
+
+//NAVIGATION MANAGEMENT
+const handleNavMobile = () => {
+	hamburger.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+	document.body.classList.toggle('ovh')
+}
+const closeNavWhenResize = () => {
+	hamburger.classList.remove('is-active')
+	navMobile.classList.remove('nav-mobile--active')
+	document.body.classList.remove('ovh')
+}
+
+sortAllBtn.addEventListener('click', sortAllProducts)
+sortCakesBtn.addEventListener('click', sortCakes)
+sortPiesBtn.addEventListener('click', sortPies)
+navMobileLinks.forEach(link => link.addEventListener('click', handleNavMobile))
 hamburger.addEventListener('click', handleNavMobile)
+
+window.addEventListener(
+	'resize',
+	throttle(closeNavWhenResize, 200)
+)
---
+++ js/slick.js
@@ -0,0 +1,39 @@
+$('.reviews-box').slick({
+    arrows: false,
+    dots: false,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
